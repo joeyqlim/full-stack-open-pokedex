490d49f624e2da315b3e17b9b4b7715fb0b30e99
joeyqlim <joeyqlim@gmail.com>
2021-07-28 11:40:06 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     browser: true,
     es6: true,
     "jest/globals": true,
+    commonjs: true,
+    es2021: true,
     node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
