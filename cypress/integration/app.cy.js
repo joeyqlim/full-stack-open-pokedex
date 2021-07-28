629bd03868a3599c38b1504d7dea20474ae1a526
joeyqlim <joeyqlim@gmail.com>
2021-07-28 15:48:30 +0200

    cypress e2e tests

---
+++ cypress/integration/app.cy.js
@@ -0,0 +1,17 @@
+describe("Pokedex", function () {
+  it("front page can be opened", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+});
+
+describe("Pokedex", function () {
+  it("pokemon page can be navigated to", function () {
+    // Can't visit pokemon url as we use react router
+    cy.contains("raichu").click();
+    cy.contains("static");
+  });
+});
